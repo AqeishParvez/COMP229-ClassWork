2c375959b873bfe1b42609212fdb173220b9d0c2
AqeishParvez <118199993+AqeishParvez@users.noreply.github.com>
2023-02-02 18:02:28 -0500

    COMP229 - manual server completed

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-console.log("Hello World!");
+const http = require('http');
 
-// const http = require('http');
-
-// http.createServer();
+http.createServer(
+    (req, res) => {
+         res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h2>Hello World</h2>');
+        }
+    ).listen(3000);
 
 // function myServerHandler(request, response){
-//     response.writeHead()
+//     response.writeHead(200, {'Content-Type': 'text/html'});
+//     response.end('<h1>Hello World</h1>');
 // }
+
+console.log('Server running at http://localhost:3000');
