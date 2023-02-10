f2d620fe2976330662214d740ec0355bf62bb395
AqeishParvez <118199993+AqeishParvez@users.noreply.github.com>
2023-02-09 21:32:35 -0500

    Mongoose Model and Controller Completed

--- app/app.js
+++ app/app.js
@@ -27,12 +27,22 @@ import path, {dirname} from 'path';
 import {fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Import Monguse module
+import mongoose from 'mongoose';
+
 //Configuration Module
-import { Secret } from '../config/index.js';
+import { Secret, MongoURI } from '../config/index.js';
 
 //Import Routes
 import indexRouter from '../app/routes/index.js';
 
+//Complete database configuration
+mongoose.connect(MongoURI);
+const db = mongoose.connection;
+
+//Database Listeners
+db.on('open', () => console.log(`Connected to MongoDB at ${MongoURI} `));
+db.on('error', () => console.log("Mongo Connection Error"));
 
 //Instantiate the express application
 const app = express();
--- config/index.js
+++ config/index.js
@@ -1,2 +1,3 @@
 //File Name: index.js, Student Name: Aqeish Parvez, Student ID: 301225795, Date: February 4th, 2023
 export const Secret = "MySecret";
+export const MongoURI = "mongodb://127.0.0.1/media"
