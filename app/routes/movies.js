444bbc3c7980f9052259ddce38e38725c7b6f4fc
AqeishParvez <118199993+AqeishParvez@users.noreply.github.com>
2023-02-12 17:11:03 -0500

    Authentication and Autherization Completed

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,20 +1,21 @@
 import { Router} from 'express';
 import { DisplayMoviesAddPage, DisplayMoviesList, DisplayMoviesEditPage, ProcessMoviesAddPage, ProcessMoviesDelete, ProcessMoviesEditPage } from '../controllers/movies.js';
+import { AuthGuard } from '../utils/index.js';
 
 const router = Router();
 
 //R ead
-router.get('/movie-list', DisplayMoviesList);
+router.get('/movie-list', AuthGuard, DisplayMoviesList);
 
 //C reate
-router.get('/movie-add', DisplayMoviesAddPage);
-router.post('/movie-add', ProcessMoviesAddPage);
+router.get('/movie-add', AuthGuard, DisplayMoviesAddPage);
+router.post('/movie-add', AuthGuard, ProcessMoviesAddPage);
 
 //U pdate
-router.get('/movie-edit/:id', DisplayMoviesEditPage);
-router.post('/movie-edit/:id', ProcessMoviesEditPage);
+router.get('/movie-edit/:id', AuthGuard, DisplayMoviesEditPage);
+router.post('/movie-edit/:id', AuthGuard, ProcessMoviesEditPage);
 
 //D elete
-router.get('/movie-delete/:id', ProcessMoviesDelete);
+router.get('/movie-delete/:id', AuthGuard, ProcessMoviesDelete);
 
 export default router;
